8a10cf4a618f44bb5eff420f9d2b193fd13380e7
TheComputerizer <dcpixelation42@gmail.com>
2024-02-11 04:50:14 -0600

    Update to gradle 8.6 and refactor some stuff

--- src/main/java/packagename/author/modid/core/EarlyMixinPlugin.java
+++ src/main/java/packagename/author/modid/core/EarlyMixinPlugin.java
@@ -2,23 +2,20 @@ package packagename.author.modid.core;
 
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.apache.logging.log4j.LogManager;
-import zone.rong.mixinbooter.IEarlyMixinLoader;
+import org.spongepowered.asm.mixin.Mixins;
 
 import javax.annotation.Nullable;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("unused")
-public class EarlyMixinPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
+public class EarlyMixinPlugin implements IFMLLoadingPlugin {
 
     static {
         LogManager.getLogger(EarlyMixinPlugin.class.getName()).info("Initializing early mixin plugin");
     }
 
-    @Override
-    public List<String> getMixinConfigs() {
-        return Collections.singletonList("modid.mixin.json");
+    public EarlyMixinPlugin() {
+        Mixins.addConfigurations("modid.mixin.json");
     }
 
     @Override
